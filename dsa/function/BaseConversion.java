8b783aae87abf083cf8121db9c61cfc7caa65c67
sunnykumar022 <sunnykumar00802@gmail.com>
2026-05-27 19:42:13 +0530

    Initial DSA and WebDev upload

---
+++ dsa/function/BaseConversion.java
@@ -0,0 +1,8 @@
+public class BaseConversion {
+    public static int toDecimal(String num, int currentBase){
+
+    }
+    public static String fromDecimal(int num, int newBase){
+        
+    }
+}
