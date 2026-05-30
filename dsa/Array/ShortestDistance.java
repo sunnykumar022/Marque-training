f59609391601d409288f98236e188cb4d020a2f3
sunnykumar022 <sunnykumar00802@gmail.com>
2026-05-30 07:49:04 +0530

    update

--- dsa/Array/ShortestDistance.java
+++ dsa/Array/ShortestDistance.java
@@ -1,26 +1,23 @@
+import jav  a.util.*;
 public class ShortestDistance{
     // Write a program to find out the shortest distance between two even positive integers in an array arr and print the distance. If there is one or zero even positive integer in the array then return -1.
-    public static int shortestDistance(int arr){
+    public static int shortestDistance(int arr[]){
         
         int res=arr.length;
         for(int i=0;i<arr.length;i++){
-            int firstEven=-1;
-          
             if (arr[i]%2==0){
-
-               int secEven=-1;
-               for(int j=i+1;j<n;j++){
-                
+               for(int j=i+1;j<arr.length;j++){
                    if(arr[j]%2==0){
-                   secEven=j;
-                    res=min(res,j-i);
+                    res=Math.min(res,j-i);
                    }
                }
             }
-            return -1;
+            
         }
+        return res==arr.length ? -1 : res;
     }
     public static void main(String []args){
-
+        int arr[]={1, 4, 3, 8, 5, 10};
+        System.out.println(shortestDistance(arr));
     }
 }
