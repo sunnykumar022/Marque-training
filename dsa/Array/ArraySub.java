23f639fa836b31fd1cd962b78e0b5789a7ef7f73
sunnykumar022 <sunnykumar00802@gmail.com>
2026-06-02 01:43:19 +0530

    UPdate

--- dsa/Array/ArraySub.java
+++ dsa/Array/ArraySub.java
@@ -1,8 +1,21 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class ArraySub {
-    static int[] calDiff(int a[], int b[], int n, int m) {
-    
+     public static int[] arrayDiff(int a[],int b[]){
+        int x=a.length;
+        int y=b.length;
+        int k=Math.max(x, y);
+        int borrow=0;
+        boolean flag=false;
+        for(int i=k-1;i>=0;i--){
+            
+            if(!flag){
+
+            }
+        }
+     }
+    public static int[] calDiff(int a[], int b[], int n, int m) {
+    //this methods work but if the number is neg it give extra zero instead of neg sign
       int num1=0;
       int place1=1;
       for(int i=a.length-1;i>=0;i--){
@@ -17,32 +30,47 @@ public class ArraySub {
           place2*=10;
       }
       int res=num1-num2;
+      boolean neg=false;
+      if(res<0){
+        res=-res;
+        neg=true;
+    }
     
       int res2=res;
       int count=0;
+      if(neg==true){
+        count=1;
+      }
       while(res>0){
           count++;
           res/=10;
       }
-      int finalres[]=new int[count];
+      int []finalres=new int[count];
       for(int i=count-1;i>=0;i--){
           finalres[i]=res2%10;
           res2/=10;
       }
+     
       return finalres;
   }
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    System.out.println("Enter size of num1[]:");
     int n1 = sc.nextInt();
-    int[] arr1 = new int[n1];
-    for (int i = 0; i < n1; i++) arr1[i] = sc.nextInt();
 
+    int[] arr1 = new int[n1];
+    System.out.println("Enter n elments:");
+
+    for (int i = 0; i < n1; i++) arr1[i] = sc.nextInt();
+    System.out.println("Enter size of num2");
     int n2 = sc.nextInt();
     int[] arr2 = new int[n2];
+    System.out.println("Enter m elements");
+
     for (int i = 0; i < n2; i++) arr2[i] = sc.nextInt();
-    sc.close();
+    
 
     int[] res = calDiff(arr1, arr2, n1, n2);
-    for (int i : res) System.out.println(i);
+    for (int i : res) System.out.print(i);
   }
 }
---
+++ dsa/Array/tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+ // static int[] arrayDiff(int a[],int b[],int n,int m){
+    //     int size=Math.max(n,m);
+    //     int res[]=new res[size];
+    //     int borrow=0;
+    //     int i=
+    // }
